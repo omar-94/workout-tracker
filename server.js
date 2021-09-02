b85d68cd82df24d486ae9bab812cd92793b4d4fa
onava <enava23@yahoo.com>
2021-09-02 02:01:42 -0400

    morgan required

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+
 
 const PORT = process.env.PORT || 3000
 
@@ -7,8 +10,10 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("combined"));
 
 app.use(express.static("public"));
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
 {
