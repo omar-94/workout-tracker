5687dbc7146b9faa5f41339df3b9a4d4396c73fd
onava <enava23@yahoo.com>
2021-09-03 00:44:21 -0400

    workout schema created

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema ({
-
-})
+    day: {
+        type: Date,
+        defaults: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String,
+                trim: true
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+        }
+    ]  
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-
+// GET Route for /api/workouts
+router.get
 
 module.exports = router;
