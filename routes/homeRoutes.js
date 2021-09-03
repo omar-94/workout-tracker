7e7da7a716ecb72a48584ddc3a17fa6df6be2a25
onava <enava23@yahoo.com>
2021-09-03 00:05:38 -0400

    exercise route added

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -4,6 +4,13 @@ const path = require("path")
 // Home Page
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
-} )
+})
+
+// Exercise Page
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+})
+
+
 
 module.exports = router;
