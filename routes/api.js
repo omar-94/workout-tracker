6d1ba41efb321416662f261ab23f8f6161e30fbe
onava <enava23@yahoo.com>
2021-09-03 01:34:09 -0400

    update get api/workouts / added PUT route

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const Workout = require("../models/workout");
 
 // GET Route for /api/workouts
-router.get('/api/workouts', (req, res ) => {
-    Workout.find([
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
         {
             $addFields: {totalDuration: {$sum: '$excercises.duration'}}
         }
@@ -17,5 +17,18 @@ router.get('/api/workouts', (req, res ) => {
         res.status(400).json(err);
     })
 });
+
+// PUT Route for /api/workouts/:id
+router.get('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}, {new: true, runValidators: true})
+    .then (dbWorkout => {
+        res.status(200).json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
+
 
 module.exports = router;
