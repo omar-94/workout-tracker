8429f5e2204219c81420c4e388efe574a1c0815f
onava <enava23@yahoo.com>
2021-09-02 23:58:02 -0400

    home page route added

---
+++ routes/api.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-
-
+const router = require("./routes/homeRoutes");
 
 const PORT = process.env.PORT || 3000
 
@@ -11,9 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("combined"));
-
 app.use(express.static("public"));
-
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
 {
@@ -24,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(router)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
